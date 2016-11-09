36aa194418d3821630d9f4591836ef3cd1fbc823
ditch0 <ditch0@users.noreply.github.com>
2016-11-09 23:30:40 +0300

    Add validations and relations to Question and Answer models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,5 @@
 class Answer < ApplicationRecord
+  validates_presence_of :body
+  validates_presence_of :question
+  belongs_to :question
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,5 @@
 class Question < ApplicationRecord
+  validates_presence_of :title
+  validates_presence_of :body
+  has_many :answers
 end
