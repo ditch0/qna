c26d49f390ae09afc00b1dd2309149250d618558
ditch0 <ditch0@users.noreply.github.com>
2016-11-12 16:47:08 +0300

    Set test framework options for generators

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,16 @@ module Qna
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixture: true,
+                       controller_spec: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       request_specs: false
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
   end
 end
