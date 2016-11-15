cad95f946de16661983dc1f53ad908092396fbc6
ditch0 <ditch0@users.noreply.github.com>
2016-11-15 22:49:46 +0300

    Fix sign in specs

--- spec/feature/sign_in_spec.rb
+++ spec/feature/sign_in_spec.rb
@@ -4,20 +4,20 @@ feature 'Sign in' do
   given!(:user) { create(:user) }
 
   scenario 'user signs in with valid credentials' do
-    visit new_session_path
+    visit new_user_session_path
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
-    click_on 'Sign in'
+    click_on 'Log in'
 
     expect(page).to have_content('Signed in successfully.')
   end
 
   scenario 'user signs in with invalid credentials' do
-    visit new_session_path
+    visit new_user_session_path
     fill_in 'Email', with: 'does.not.exist@somewhere.com'
     fill_in 'Password', with: 'qwerty'
-    click_on 'Sign in'
+    click_on 'Log in'
 
-    expect(page).to have_content('Invalid email or password.')
+    expect(page).to have_content('Invalid Email or password.')
   end
 end
