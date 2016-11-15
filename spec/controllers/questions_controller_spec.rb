9665b0679da52b19e7f2bd7fb218803531dc6024
ditch0 <ditch0@users.noreply.github.com>
2016-11-15 21:36:33 +0300

    Create new answer model in questions#show

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -24,6 +24,10 @@ RSpec.describe QuestionsController, type: :controller do
     it 'assigns @question' do
       expect(assigns(:question)).to eq(question)
     end
+
+    it 'assigns new answer model to @answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
   end
 
   describe 'GET #new' do
