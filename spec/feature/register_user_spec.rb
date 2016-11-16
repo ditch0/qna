c9b3ede283d1cc2f0bfffb167d14be2f9f138059
ditch0 <ditch0@users.noreply.github.com>
2016-11-16 20:18:58 +0300

    Test user registration

---
+++ spec/feature/register_user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'User registration' do
+  given(:user) { attributes_for(:user) }
+
+  scenario 'user registers' do
+    visit new_user_registration_path
+    fill_in 'Email', with: user[:email]
+    fill_in 'Password', with: user[:password]
+    fill_in 'Password confirmation', with: user[:password_confirmation]
+    click_on 'Sign up'
+
+    expect(page).to have_content('Welcome! You have signed up successfully.')
+  end
+end
