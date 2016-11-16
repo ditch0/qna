ad1ec43fba749cc3fcd91023300f9d2c3baf8b39
ditch0 <ditch0@users.noreply.github.com>
2016-11-16 21:47:22 +0300

    Require user authentication to create answer

--- spec/feature/create_answer_spec.rb
+++ spec/feature/create_answer_spec.rb
@@ -2,13 +2,22 @@ require 'rails_helper'
 
 feature 'User can answer a question' do
   given!(:question) { create(:question) }
+  given!(:user) { create(:user) }
 
-  scenario 'create answer' do
+  scenario 'authorized user creates answer' do
+    sign_in user
     visit question_path(question)
     fill_in 'Your answer', with: 'My answer'
     click_on 'Submit'
 
     expect(page).to have_current_path(question_path(question))
     expect(page).to have_content('My answer')
+  end
+
+  scenario 'unauthorized user cannot see answer form' do
+    visit question_path(question)
+
+    expect(page).not_to have_field('My answer')
+    expect(page).not_to have_button('Submit')
   end
 end
