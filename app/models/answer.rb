7e95f62a6b67a26002a1cc8d9db05a0d84c35cd2
ditch0 <ditch0@users.noreply.github.com>
2016-11-26 19:27:58 +0300

    Make "best answer" a property of an answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,14 @@ class Answer < ApplicationRecord
   validates :body, presence: true
   belongs_to :user
   belongs_to :question
+
+  before_save :unset_previous_best_answer, if: :is_best
+
+  private
+
+  def unset_previous_best_answer
+    question.answers.where(is_best: true).each do |answer|
+      answer.update_attributes(is_best: false)
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161124192218) do
+ActiveRecord::Schema.define(version: 20161126150906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
-    t.text     "body",        null: false
-    t.integer  "question_id", null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.text     "body",                        null: false
+    t.integer  "question_id",                 null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.integer  "user_id"
+    t.boolean  "is_best",     default: false
     t.index ["question_id"], name: "index_answers_on_question_id", using: :btree
     t.index ["user_id"], name: "index_answers_on_user_id", using: :btree
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string   "title",          null: false
-    t.text     "body",           null: false
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.string   "title",      null: false
+    t.text     "body",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "user_id"
-    t.integer  "best_answer_id"
-    t.index ["best_answer_id"], name: "index_questions_on_best_answer_id", using: :btree
     t.index ["user_id"], name: "index_questions_on_user_id", using: :btree
   end
 
@@ -54,6 +53,5 @@ ActiveRecord::Schema.define(version: 20161124192218) do
   end
 
   add_foreign_key "answers", "users"
-  add_foreign_key "questions", "answers", column: "best_answer_id"
   add_foreign_key "questions", "users"
 end
