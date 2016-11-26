f9fb884e7ccb0a1f44d10be191b3119dd6e82e04
ditch0 <ditch0@users.noreply.github.com>
2016-11-26 22:43:02 +0300

    Update best answer in transaction

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,9 +6,10 @@ class Answer < ApplicationRecord
   scope :best_and_newest_order, -> { order(is_best: :desc, id: :desc) }
 
   def update_is_best(is_best)
-    question.answers.update_all(is_best: false)
-    return unless is_best
-    self.is_best = true
-    save
+    ActiveRecord::Base.transaction do
+      question.answers.update_all(is_best: false)
+      self.is_best = is_best
+      save
+    end
   end
 end
