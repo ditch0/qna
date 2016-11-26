2ced6b43e618330ce609838fd80095fb12f3cb13
ditch0 <ditch0@users.noreply.github.com>
2016-11-26 22:22:36 +0300

    Make best answer column non-nullable

--- db/migrate/20161126150906_add_is_best_to_answer.rb
+++ db/migrate/20161126150906_add_is_best_to_answer.rb
@@ -1,5 +1,5 @@
 class AddIsBestToAnswer < ActiveRecord::Migration[5.0]
   def change
-    add_column :answers, :is_best, :boolean, default: false
+    add_column :answers, :is_best, :boolean, default: false, null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20161126150906) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.integer  "user_id"
-    t.boolean  "is_best",     default: false
+    t.boolean  "is_best",     default: false, null: false
     t.index ["question_id"], name: "index_answers_on_question_id", using: :btree
     t.index ["user_id"], name: "index_answers_on_user_id", using: :btree
   end
