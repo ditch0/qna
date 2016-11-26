62dc0ed2250e20d6ee074664c16f29bbb4c05405
ditch0 <ditch0@users.noreply.github.com>
2016-11-26 20:43:44 +0300

    Make Rubocop happier

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,12 +5,12 @@ class Answer < ApplicationRecord
 
   before_save :unset_previous_best_answer, if: :is_best
 
-  scope :best_and_newest_order, ->{ order(is_best: :desc, id: :desc) }
+  scope :best_and_newest_order, -> { order(is_best: :desc, id: :desc) }
 
   private
 
   def unset_previous_best_answer
-    question.answers.where(is_best: true).each do |answer|
+    question.answers.where(is_best: true).find_each do |answer|
       answer.update_attributes(is_best: false)
     end
   end
