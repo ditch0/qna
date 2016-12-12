4b583e7c288c95e2dbd4fee432faa781236dfe55
ditch0 <ditch0@users.noreply.github.com>
2016-12-12 21:53:58 +0300

    Remove redundant required: false

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,6 @@
 class Vote < ApplicationRecord
   validates :value, presence: true, inclusion: [1, -1]
   validates :user_id, uniqueness: { scope: [:votable_type, :votable_id] }
-  belongs_to :user, required: false
-  belongs_to :votable, polymorphic: true, required: false
+  belongs_to :user
+  belongs_to :votable, polymorphic: true
 end
