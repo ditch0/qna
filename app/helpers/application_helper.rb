22d8a9967bce4f622e1aa2a6208baf1226bed13c
ditch0 <ditch0@users.noreply.github.com>
2017-01-28 15:39:06 +0300

    Remove collection_cache_key_for and use default Rails cache key calculation for models collections

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,15 +14,4 @@ module ApplicationHelper
                end
     link_to question.title, question_path(question)
   end
-
-  def collection_cache_key_for(model_type, parent_model = nil)
-    klass = model_type.to_s.capitalize.constantize
-    max_updated_at = klass.maximum(:updated_at).try(:utc).try(:to_s, :number)
-    model_key = "#{model_type.to_s.pluralize}/collection-#{klass.count}-#{max_updated_at}"
-    return model_key if parent_model.nil?
-
-    parent_model_type = parent_model.class.to_s.underscore
-    parent_model_key = "#{parent_model_type.pluralize}/#{parent_model.id}/#{model_key}"
-    "#{parent_model_key}/{model_key}"
-  end
 end
