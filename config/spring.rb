142e721472a1bab8b5c2fe08b6cacc89e07094c2
ditch0 <ditch0@users.noreply.github.com>
2018-05-01 14:34:40 +0300

    Update Ruby and Rails

--- config/spring.rb
+++ config/spring.rb
@@ -1,24 +1,6 @@
-require 'spring/application'
-
-class Spring::Application
-  alias connect_database_orig connect_database
-
-  def connect_database
-    disconnect_database
-    reconfigure_database
-    connect_database_orig
-  end
-
-  def reconfigure_database
-    if active_record_configured?
-      ActiveRecord::Base.configurations = Rails.application.config.database_configuration
-    end
-  end
-end
-
-%w(
+%w[
   .ruby-version
   .rbenv-vars
   tmp/restart.txt
   tmp/caching-dev.txt
-).each { |path| Spring.watch(path) }
+].each { |path| Spring.watch(path) }
